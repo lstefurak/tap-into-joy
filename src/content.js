c06aca0bcf9b7bf167bbf47fc5811a458df68b3d
Luke Stefurak <lstefurak@gmail.com>
2026-08-06 10:26:20 -0700

    Update content.js
    Update Grammer

--- src/content.js
+++ src/content.js
@@ -16,8 +16,8 @@ export const content = {
   about: {
     heading: "Meet Sue Doherty",
     paragraphs: [
-      "Welcome! I'm Sue Doherty, a certified clinical EFT (Emotional Freedom Techniques) practitioner dedicated to helping you to emotional regulation. Find the peace within yourself; See your limiting beliefs clarity; and step into your power."
-      Sue's work as a neuroscientist at NIMH was foundational to her linking spiritual developement with neuroplasticit. EFT technique allowed her me to release emotional blocks and shine as a premium coach. Through your sessions wtih Sue, you'll use the tools to down-regulate anxiety, overwhelm, fear and self-doubt so you too can step into your own transformation 
+      "Welcome! I'm Sue Doherty, a certified clinical EFT (Emotional Freedom Techniques) practitioner dedicated to helping you to emotional regulation. Find the peace within yourself; See your limiting beliefs clarity; and step into your power.",
+      "Sue's work as a neuroscientist at NIMH was foundational to her linking spiritual developement with neuroplasticit. EFT technique allowed her me to release emotional blocks and shine as a premium coach. Through your sessions wtih Sue, you'll use the tools to down-regulate anxiety, overwhelm, fear and self-doubt so you too can step into your own transformation 
     ]
   },
 
